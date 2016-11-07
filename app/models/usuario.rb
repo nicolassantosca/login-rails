a67e4acd9263164f4e23d4bdf4f19609f9efd408
Nicolas Santos <santos.ras12@gmail.com>
2016-11-07 17:38:48 -0500

    cree login

---
+++ app/models/usuario.rb
@@ -0,0 +1,9 @@
+class Usuario < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+        #  protect_from_forgery prepend: true
+
+
+end
